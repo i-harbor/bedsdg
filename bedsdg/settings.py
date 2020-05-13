d59c8c717a5f0d917d025c62193b90c2fa544152
shun <869588058@qq.com>
2020-05-13 14:19:35 +0800

    配置mysql

--- bedsdg/settings.py
+++ bedsdg/settings.py
@@ -87,10 +87,20 @@ WSGI_APPLICATION = 'bedsdg.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
+        'NAME': 'bedsdg',       # 数据的库名，事先要创建之
+        'HOST': '127.0.0.1',    # 主机
+        'PORT': '3306',         # 数据库使用的端口
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': '',  # 密码
+        'CONN_MAX_AGE': 3600,   # 1h, None用于无限的持久连接
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
+    },
 }
 
 
